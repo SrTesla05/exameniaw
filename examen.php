8a2e0ac0e38257808378edb6e93cd84012bc93cd
adrian <adrian@adrian.com>
2024-10-31 09:12:35 +0000

    Segundo Commit

--- examen.php
+++ examen.php
@@ -1,4 +1,7 @@
-<?php
-echo 'Adrián Carrillo - 31/10/2024';
-?>
-<h1><?php echo 'Un repositorio de código es un sitio donde podemos guardar códigos e ir haciendo versiones'?></h1>
+<html>
+<body>
+<h1><?php echo 'Adrián CArrillo'?></h1>
+<h2><?php echo '31/10/2024'?></h2>
+<h3><?php echo 'Un repositorio de código es un sitios donde podemos guaradr códigos e ir haciendo versiones'?></h3>
+</body>
+</html>
